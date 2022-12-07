21032118ef98b3328bfe23983f2d8ac568a32110
JINBOK LEE <eyelash1024@naver.com>
2022-12-08 00:26:10 +0900

    file reorganization

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
-// const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const mongoose = require("mongoose");
-// const { User } = require("./models/User");
-// const { Product } = require("./models/Product");
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -22,19 +18,7 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-// mongoose.connect(process.env.MONGO_URI);
-// const db = mongoose.connection;
-// db.on("error", (error) => console.log(error));
-// db.once("open", () => console.log(`Connected to mongoDB`));
-
 app.use(cors());
-
-// //to not get any deprecation warning or error
-// //support parsing of application/x-www-form-urlencoded post data
-// app.use(bodyParser.urlencoded({ extended: true }));
-// //to get json data
-// // support parsing of application/json type post data
-// app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -46,7 +30,6 @@ app.use("/api/products", require("./routes/products"));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use("/temp", express.static("temp"));
-app.use(express.static(__dirname + "/public"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
@@ -54,13 +37,15 @@ if (process.env.NODE_ENV === "production") {
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
+  // index.html for all page routes html
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
+} else {
+  app.use(express.static(__dirname + "/public"));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`);
